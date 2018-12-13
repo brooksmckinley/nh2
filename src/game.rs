d4a533e0973ca395fdbbf8661d91ae82ebe65ff5
Brooks <brooksmckinley@yahoo.com>
2018-12-13 10:40:42 -0500

    Added register and play functions to game.rs

--- src/game.rs
+++ src/game.rs
@@ -1,5 +1,33 @@
 use db::User;
+use std::process::Command;
+use std::env;
 
 pub fn play_game(user: &User) {
-    unimplemented!()
+    // Check for currently running sessions to re-attach
+    let reattach = Command::new("screen")
+        .arg("-dr")
+        .status()
+        .unwrap();
+    // If there were none, start the game again
+    if !reattach.success() {
+        Command::new("screen")
+            .arg("-S")
+            .arg(&user.name)
+            .arg("nethack")
+            .arg("-d")
+            .arg(format!("{}/{}", env::var("PWD").unwrap(), user.name))
+            .arg("-u")
+            .arg(&user.name)
+            .status()
+            .unwrap();
+    }
+}
+
+pub fn generate_skeleton(user: &User) {
+    Command::new("/bin/cp")
+        .arg("-r")
+        .arg("./skeleton")
+        .arg(user.name.clone())
+        .output()
+        .unwrap();
 }
